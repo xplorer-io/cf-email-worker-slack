0fb814d74fc9fe8cee50c07d3c7849aca17b342b
yash shrestha <yashcrest@gmail.com>
2024-10-03 23:13:05 +1000

    adding email worker logic

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,53 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as PostalMime from 'postal-mime';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async email(message: EmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
+		await handleEmail(message, env, ctx);
 	},
-} satisfies ExportedHandler<Env>;
+};
+
+async function handleEmail(message: EmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
+	const parser = new PostalMime.default();
+	const slackWebhookUrl = env.SLACK_WEBHOOK_URL;
+
+	if (!slackWebhookUrl) {
+		throw new Error('SLACK_WEBHOOK_URL is missing in environment variables.');
+	}
+
+	try {
+		//parse email content
+		const rawEmail = new Response(message.raw);
+
+		const email = await parser.parse(await rawEmail.arrayBuffer());
+
+		//get attachments
+		let emailAttachments = [];
+		email.attachments.forEach((attachment) => {
+			let decoder = new TextDecoder('utf-8');
+			emailAttachments.push(decoder);
+		});
+
+		const from = message.from;
+		const to = message.to;
+		const subject = message.headers.get('subject') || '(no subject)';
+
+		const slackMessage = {
+			text: `📧 *New Email Received* \n\n*From:* ${from}\n*To:* ${to}\n*Subject:* ${subject}\n\n*Raw Email:*\n\`\`\`${rawEmail}\`\`\``,
+		};
+
+		//forwarding email to slack
+		const response = await fetch(slackWebhookUrl, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(slackMessage),
+		});
+
+		if (!response.ok) {
+			throw new Error(`Slack Webhook Error ${response.statusText}`);
+		}
+
+		console.log('Email forwarded to Slack Successfully!');
+	} catch (error) {
+		console.log('Error handling email: ', error);
+	}
+}
